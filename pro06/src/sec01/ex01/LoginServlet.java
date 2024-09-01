d2ea84f957671b201d7f99843ce8f791f293eab0
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2024-09-01 17:43:32 +0900

    add : 8장

--- pro06/src/sec01/ex01/LoginServlet.java
+++ pro06/src/sec01/ex01/LoginServlet.java
@@ -2,12 +2,11 @@ package sec01.ex01;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
+//@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     public void init() throws ServletException {
