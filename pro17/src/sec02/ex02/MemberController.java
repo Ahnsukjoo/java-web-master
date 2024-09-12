e5bc33e8e85df1a90021f6757c774e9ef19ab10b
Ahnsukjoo <bos98@naver.com>
2024-09-12 17:42:06 +0900

    add : 수정, 삭제 구현

--- pro17/src/sec02/ex02/MemberController.java
+++ pro17/src/sec02/ex02/MemberController.java
@@ -1,14 +1,15 @@
-package sec02.ex01;
+package sec02.ex02;
 
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/*@WebServlet("/member/*")*/
+@WebServlet("/member/*")
 public class MemberController extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -41,7 +42,7 @@ public class MemberController extends HttpServlet {
         if (action == null || action.equals("/listMembers.do")) {
             List<MemberVO> membersList = memberDAO.listMembers();
             request.setAttribute("membersList", membersList);
-            nextPage = "/test02/listMembers.jsp";
+            nextPage = "/test03/listMembers.jsp";
         } else if (action.equals("/addMember.do")) {
             String id = request.getParameter("id");
             String pwd = request.getParameter("pwd");
@@ -51,11 +52,34 @@ public class MemberController extends HttpServlet {
             memberDAO.addMember(memberVO);
             nextPage = "/member/listMembers.do";
         } else if (action.equals("/memberForm.do")) {
-            nextPage = "/test02/memberForm.jsp";
+            nextPage = "/test03/memberForm.jsp";
+        } else if (action.equals("/modMemberForm.do")) {
+            String id = request.getParameter("id");
+            MemberVO memInfo = memberDAO.findMember(id);
+            request.setAttribute("memInfo", memInfo);
+            nextPage = "/test03/modMemberForm.jsp";
+        } else if (action.equals("/modMember.do")) {
+            String id = request.getParameter("id");
+            String pwd = request.getParameter("pwd");
+            String name = request.getParameter("name");
+            String email = request.getParameter("email");
+            MemberVO memberVO = new MemberVO(id, pwd, name, email);
+            memberDAO.modMember(memberVO);
+            request.setAttribute("msg", "modified");
+            nextPage = "/member/listMembers.do";
+
+
+        } else if (action.equals("/delMember.do")) {
+            String id = request.getParameter("id");
+            memberDAO.delMember(id);
+            request.setAttribute("msg", "deleted");
+            nextPage = "/member/listMembers.do";
+
+
         } else {
             List<MemberVO> membersList = memberDAO.listMembers();
             request.setAttribute("membersList", membersList);
-            nextPage = "/test02/listMembers.jsp";
+            nextPage = "/test03/listMembers.jsp";
         }
         RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
         dispatch.forward(request, response);
